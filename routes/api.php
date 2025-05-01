48f712770424930d51a8fa2f62e87076c9889cdd
João Pedro Leal <168142462+Joaoplsr@users.noreply.github.com>
2025-05-01 06:57:27 -0300

    Rotas de listagem de artigos

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ArticleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -12,9 +13,28 @@ Route::controller(AuthController::class)
     Route::post('/login', 'login')->name('api.auth.login');
 });
 
+// Rotas de Artigos Públicos
+Route::controller(ArticleController::class)
+->prefix('articlesguest')
+->group(function () {
+    Route::get('/', 'index-public')->name('api.articlesguest.index-public');
+    Route::get('/{article}', 'show')->name('api.articlesguest.show');
+});
+
 // Rotas Protegidas
 Route::middleware('auth:sanctum')->group(function () {
+
     Route::post('/logout', [AuthController::class, 'logout'])->name('api.logout');
+
+    Route::controller(ArticleController::class)
+    ->prefix('articles')
+    ->group(function () {
+        //Rotas de Listagem de Artigos Privados
+        Route::get('/', 'index')->name('api.articles.index');
+        Route::get('/', 'index-inreview')->name('api.articles.index-inreview');
+        Route::get('/', 'index-reviewed')->name('api.articles.index-reviewed');
+        
+    });
 });
 
 // Route::get('/user', function (Request $request) {
