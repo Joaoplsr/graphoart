43b97563e1553dae99aab75f98451dfb60b4427f
João Pedro Leal <168142462+Joaoplsr@users.noreply.github.com>
2025-05-07 12:53:31 -0300

    Configuração das rotas de autenticação e criação da AuthController.

---
+++ routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+//Rotas Auth
+Route::controller(AuthController::class)
+->prefix('auth')
+->group(function () {
+    Route::post('/register', 'register')->name('api.auth.register');
+    Route::post('/login', 'login')->name('api.auth.login');
+});
+
+// Rotas Protegidas
+Route::middleware('auth-sanctum')->group(function () {
+    Route::post('logout', [AuthController::class, 'logout'])->name('api.logout');
+
+});
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
